75601d7a4eb012fea4b5f2330c1941bd011fdb35
Sankalpbp <sankalpnitj@gmail.com>
2022-10-19 19:23:22 +0530

    Fetching data from the API and rendering on the page

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 'use strict';
 
-function hitWeatherApi () {
-    console.log("something is working");
+const hitWeatherApi = async () => {
+    const location = 'Jalandhar';
+    const response = await fetch ('http://api.weatherstack.com/current?access_key=1033c76d0c1530af36960575ab2b03a8&query=Jalandhar');
+    const weatherData = await response.json();
 
-    // hit weather api
-
-    // new page for showing weather data
+    document.getElementById('city').innerHTML = 'City: ' + weatherData.location.name;
+    document.getElementById('region').innerHTML = 'Region: ' + weatherData.location.region;
+    document.getElementById('country').innerHTML = 'Country: ' + weatherData.location.country;
+    
+    document.getElementById('temperature').innerHTML = 'Temperature right now is: ' + weatherData.current.temperature + ', but feels like: ' + weatherData.current.feelsLike;
+    document.getElementById('feels-like').innerHTML = 'Temperature feels like: ' + weatherData.current.feelslike;
+    document.getElementById('humidity').innerHTML = 'Humidity right now is: ' + weatherData.current.humidity;
+    document.getElementById('visibility').innerHTML = 'Visibility right now is: ' + weatherData.current.visibility;
+    document.getElementById('wind-speed').innerHTML = 'Wind Speed right now is: ' + weatherData.current.wind_speed;
+    document.getElementById('wind-direction').innerHTML = 'Wind Direction right now is: ' + weatherData.current.wind_dir;
 }
